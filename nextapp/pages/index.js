0b7395b8ba509f472b9fd532d20a514a730f0904
Pragya Agrawal <pragyachik@gmail.com>
2022-10-04 19:40:54 +0530

    add next link

--- nextapp/pages/index.js
+++ nextapp/pages/index.js
@@ -2,12 +2,13 @@ import Head from 'next/head'
 import Image from 'next/image'
 // import styles from '../styles/Home.module.css'
 import plorusStyles from '../styles/Plorus.module.css'
+import Link from "next/link"
 
 export default function Home() {
   return (
     <div className={plorusStyles.container}>
       <h1 className={plorusStyles.title}>Homepage</h1>
-      <a href="/about">About</a>
+      <Link href="/about"><a>About</a></Link>
     </div>
   )
 }
