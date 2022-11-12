919ebfef8f0d0047ef1c85a08cb2a2a02ec190b0
Pragya Agrawal <pragyachik@gmail.com>
2022-11-12 17:56:55 +0530

    connect to firestore, add and display post entries

---
+++ nextapp/utils/db/index.js
@@ -0,0 +1,16 @@
+import admin from 'firebase-admin';
+import serviceAccount from './serviceAccountKey.json';
+
+if (!admin.apps.length) {
+  try {
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount)
+    });
+  } catch (error) {
+    console.log('Firebase admin initialization error', error.stack);
+  }
+}
+
+const db = admin.firestore()
+
+export default db;
