c935ae9e9947f9f3722cb170649dcd707c6fe5fe
Pragya Agrawal <pragyachik@gmail.com>
2022-12-19 20:26:48 +0530

    remove firebase apis

--- nextapp/pages/posts/index.js
+++ nextapp/pages/posts/index.js
@@ -1,32 +1,32 @@
-import Link from 'next/link'
-import db from '../../utils/db';
+// import Link from 'next/link'
+// import db from '../../utils/db';
 
-export const getStaticProps = async () => {
-  const entries = await db.collection('entries').orderBy('created', 'desc').get();
-  const entriesData = entries.docs.map(entry => ({
-    id: entry.id,
-    ...entry.data()
-  }));
+// export const getStaticProps = async () => {
+//   const entries = await db.collection('entries').orderBy('created', 'desc').get();
+//   const entriesData = entries.docs.map(entry => ({
+//     id: entry.id,
+//     ...entry.data()
+//   }));
 
-  return {
-    props: { entriesData },
-    revalidate: 10
-  }
-}
+//   return {
+//     props: { entriesData },
+//     revalidate: 10
+//   }
+// }
 
-export default function Posts ({entriesData}) {
+// export default function Posts ({entriesData}) {
 
-  return (
-    <div>
-      <h1>Posts</h1>
-      {entriesData.map(entry => (
-        <div key={entry.id}>
-          <Link href={`/posts/${entry.slug}`}>
-            <a>{entry.title}</a>
-          </Link>
-          <br />
-        </div>
-      ))}
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <h1>Posts</h1>
+//       {entriesData.map(entry => (
+//         <div key={entry.id}>
+//           <Link href={`/posts/${entry.slug}`}>
+//             <a>{entry.title}</a>
+//           </Link>
+//           <br />
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
