211b50b37b3584c858fda75de36410ecf66780d7
Pragya Agrawal <pragyachik@gmail.com>
2023-04-14 01:49:04 +0530

    UI, navbar

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -2,7 +2,24 @@
 const HeroSection = () => {
   return (
     <>
-      <div className="h-[300vh] w-full bg-gradient-to-r from-indigo-200 from-5% via-sky-200 via-30% to-emerald-200 to-90%"></div>
+      <div className="flex flex-col h-[300vh] w-full">
+        <div className="h-[100vh] w-full bg-gradient-to-r from-indigo-400 from-5% via-sky-400 via-30% to-emerald-400 to-90%">
+          <div className="md:hidden">
+            <div className="text-6xl text-white font-bold font-sans ml-40 mt-60 w-[50vw]">
+              Generate Academic Journals and assorted AI media
+            </div>
+            <div className="text-xl text-white font-mono ml-40 mt-10 w-[50vw]">
+              Find all our varied offerings on the documentation page
+            </div>
+          </div>
+        </div>
+        <div className="h-[100vh] w-full bg-white">
+
+        </div>
+        <div className="h-[100vh] w-full bg-slate-200">
+
+        </div>
+      </div>
     </>
   )
 }
--- components/Navbar.js
+++ components/Navbar.js
@@ -4,7 +4,7 @@ import { useScroll, useMotionValueEvent } from "framer-motion"
 
 const Navbar = () => {
   const { scrollY } = useScroll()
-  const [scrollPosition, setScrollPosition] = useState(false)
+  const [isScrolled, setScrollPosition] = useState(false)
 
   useMotionValueEvent(scrollY, "change", (latest) => {
     if(latest>0){
@@ -16,11 +16,52 @@ const Navbar = () => {
     }
   })
 
+  //bg-[rgba(255,255,255,0)]
+  //style={{"font-variation-settings":"'cuts' 100, 'move' 100","font-variant-ligatures":"common-ligatures"}}
+
   return (
     <>
     {/* <nav className={"h-[14vh] w-full "+(scrollPosition?"shadow-[0px_4px_20px_0px_rgba(0,0,0,.15)] bg-[rgba(255,255,255,0.5)] border-none":" border-b-2 border-b-white")}>
     </nav> */}
-    <nav className={"fixed top-0 left-0 h-[14vh] w-full "+(scrollPosition?"shadow-[0px_4px_20px_0px_rgba(0,0,0,.15)] bg-[rgba(255,255,255,0.6)] border-none":"bg-[rgba(255,255,255,0)]  border-b-2 border-b-white")}>
+    <nav className={"flex flex-row justify-between pl-12 pr-12 items-center fixed top-0 left-0 h-[10vh] w-full "+(isScrolled?"shadow-[0px_4px_20px_0px_rgba(0,0,0,.15)] bg-[rgba(255,255,255,0.8)] border-none":"bg-[rgba(0,0,0,0.15)]  border-b-2 border-b-white")}>
+      <div className={"text-3xl font-mono "+(isScrolled?" text-black":" text-white")}>
+        Plorus
+      </div>
+      <div className={"md:hidden flex flex-row space-between font-mono text-xl"+(isScrolled?" text-black":" text-white")}>
+        <div className={"pl-4 pr-4 hover:drop-shadow hover:cursor-pointer "+(isScrolled?" hover:text-emerald-500":" hover:text-emerald-200")}>
+          About
+        </div>
+        <div className={"pl-4 pr-4 hover:drop-shadow hover:cursor-pointer "+(isScrolled?" hover:text-emerald-500":" hover:text-emerald-200")}>
+          Pricing
+        </div>
+        <div className={"pl-4 pr-4 hover:drop-shadow hover:cursor-pointer "+(isScrolled?" hover:text-emerald-500":" hover:text-emerald-200")}>
+          Blog
+        </div>
+        <div className={"pl-4 pr-4 hover:drop-shadow hover:cursor-pointer"+(isScrolled?" hover:text-emerald-500":" hover:text-emerald-200")}>
+          Documentation
+        </div>
+      </div>
+      <div className={"hidden md:block "}>
+        <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="w-10 h-10"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke={isScrolled?"black":"white"}
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M4 6h16M4 12h16M4 18h16"
+        />
+      </svg>
+      </div>
+      {/* <div class="hidden md:block space-y-2">
+        <div class={"w-8 h-0.5 "+(isScrolled?"bg-black":"bg-white")}></div>
+        <div class={"w-8 h-0.5 "+(isScrolled?"bg-black":"bg-white")}></div>
+        <div class={"w-8 h-0.5 "+(isScrolled?"bg-black":"bg-white")}></div>
+      </div> */}
     </nav>
     </>
   )
