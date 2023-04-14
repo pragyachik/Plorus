c79dcfa609b1dbbb8bb5489469bd0b803e0e6ef5
Pragya Agrawal <pragyachik@gmail.com>
2023-04-15 02:28:37 +0530

    UI, responsive

--- components/Footer.js
+++ components/Footer.js
@@ -3,8 +3,8 @@ const Footer = () => {
   return (
     <>
       <div className="h-[70vh] w-full bg-gray-800 pt-8 pb-8 flex flex-col justify-between px-12">
-        <div className="pl-12 pr-12  flex flex-row justify-between h-[55vh]">
-
+        <div className="pl-12 pr-12 w-full flex flex-row justify-between h-[55vh]">
+          
         </div>
         <div className="my-4 border-t border-t-volcanic-800 md:hidden "></div>
         <div className="pl-12 pr-12 flex flex-row justify-between text-white md:hidden ">
--- components/HeroSection.js
+++ components/HeroSection.js
@@ -1,18 +1,29 @@
+import Image from 'next/image'
+// import dashboardImage from "../public/dashboard_placeholder.png"
 
 const HeroSection = () => {
   return (
     <>
       <div className="flex flex-col h-[300vh] w-full">
         <div className="h-[100vh] w-full bg-gradient-to-r md:bg-gradient-to-b from-indigo-400 from-5% via-sky-400 via-30% to-emerald-400 to-90%">
-          <div className="mt-[10vh] h-[90vh] w-full bg-my_bg_image bg-cover bg-center bg-no-repeat">
-            <div className="md:hidden">
-              <div className="text-6xl text-white font-bold font-sans pl-[10vw] pt-[25vh] w-[60vw]">
+          <div className="mt-[10vh] h-[90vh] w-full bg-my_bg_image bg-cover bg-center bg-no-repeat flex flex-row">
+            <div className="">
+              <div className="text-6xl text-white font-bold font-sans pl-[10vw] pt-[25vh] w-[60vw] lg:w-[90vw] md:pt-[15vh] md:text-5xl">
                 Generate Academic Journals and assorted AI media
               </div>
-              <div className="text-xl text-white font-mono pl-[10vw] pt-[5vh] w-[60vw]">
+              <div className="text-xl text-white font-mono pl-[10vw] pt-[3vh] w-[60vw] lg:w-[90vw] md:text-lg">
                 Find all our varied offerings on the documentation page
               </div>
+              <div className="pl-[10vw] pt-[5vh] flex flex-row">
+                <div>
+                  <button className="bg-slate-800 shadow-md text-white rounded-lg px-8 py-2 mr-4 text-lg font-bold font-mono hover:shadow-lg hover:bg-slate-900 md:text-base md:px-4 md:py-2 md:mr-2">Try our Demo!</button>
+                  <button className="bg-slate-200 text-black shadow-md rounded-lg px-8 py-2 ml-4 text-lg font-bold font-mono hover:shadow-lg hover:bg-slate-100 md:text-base md:px-4 md:py-2 md:ml-2">Contact Us</button>
+                </div>
+              </div>
             </div>
+            {/* <div className="md:hidden w-full h-full flex items-center justify-center mr-[10vw]">
+              <Image src="/dashboard_placeholder.png" width={1200} height={800} />
+            </div> */}
           </div>
           
         </div>
