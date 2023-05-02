97aef9f601dcc3d0740e36296e5790a71280d68a
Pragya Agrawal <pragyachik@gmail.com>
2023-05-02 17:37:09 +0530

    add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,33 @@
+import Head from 'next/head'
+import Script from 'next/script'
 import '../styles/globals.css'
 import 'antd/dist/antd.css';
+import * as gtag from '../lib/gtag'
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', ${gtag.GA_TRACKING_ID});
+            `,
+          }}
+        />
+      </Head>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
