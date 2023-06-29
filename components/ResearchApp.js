cde5e49b3ba6f81019ceef567f778d47fde6d5fd
Pragya Agrawal <pragyachik@gmail.com>
2023-06-29 16:44:42 +0530

    Red Pajama, tokens

--- components/ResearchApp.js
+++ components/ResearchApp.js
@@ -18,7 +18,7 @@ const ComponentSelector = () => {
       case 'Bert':
         return <Bert />;
       default:
-        return null;
+        return <TokenOutput />;
     }
   };
 
@@ -26,7 +26,7 @@ const ComponentSelector = () => {
     <div className="h-[92vh] w-full bg-white p-auto flex flex-col font-mono font-semibold text-black">
       <div className='h-[7vh] p-2'>
         <select value={selectedComponent} onChange={handleComponentChange}>
-          <option value="">Select a Model</option>
+          <option value=" ">Select a Model</option>
           <option value="Bert">Bert</option>
           <option value="Red Pajama">Red Pajama</option>
           <option value="Stable Diffusion">Stable Diffusion</option>
@@ -42,7 +42,48 @@ const ComponentSelector = () => {
 
 // Example components
 const RedPajama = () => {
-  return <div>Red Pajama</div>;
+  const [showButton, setShowButton] = useState(true)
+  const [userRequest, setUserRequest] = useState("test")
+  const [response, setResponse] = useState("response here")
+
+  const getVal = (value) => {
+    setUserRequest(value.target.value)
+  }
+
+  const callAPI = async () => {
+    setShowButton(false)
+
+    let prompt = userRequest
+
+    const params = {
+      text: prompt
+    }; 
+
+    const options = {
+        method: 'POST',
+        body: JSON.stringify( params )  
+    };
+
+    //http://172.105.52.83
+    const response = await fetch("http://127.0.0.1:5000/api/getRedPajamaToken", options);
+    // console.log(response)
+
+    const responseData = await response.json()
+
+    setShowButton(true)
+    setResponse(JSON.stringify(responseData))
+  }
+  
+  return (
+    <div className='flex flex-col justify-around h-[50%]'>
+      <div>Red Pajama</div>
+      <div className="chatbox__chatarea">
+        <textarea className="chatbox__textarea border-2 border-slate-400" onChange={getVal} placeholder="enter text here" rows="3" cols="30"></textarea>
+        {showButton? <><br></br><button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow" onClick={()=>callAPI()}>get response</button></> : <div>Loading...</div>}
+      </div>
+      <div>{response}</div>
+    </div>
+  );
 };
 
 const StableDiffusion = () => {
@@ -57,5 +98,50 @@ const Wav2 = () => {
 const Bert = () => {
   return <div>Bert</div>;
 };
+
+const TokenOutput = () => {
+  const [showButton, setShowButton] = useState(true)
+  const [userRequest, setUserRequest] = useState("test")
+  const [response, setResponse] = useState("response here")
+
+  const getVal = (value) => {
+    setUserRequest(value.target.value)
+  }
+
+  const callAPI = async () => {
+    setShowButton(false)
+
+    let prompt = userRequest
+
+    const params = {
+      token: prompt
+    }; 
+
+    const options = {
+        method: 'POST',
+        body: JSON.stringify( params )  
+    };
+
+    //http://172.105.52.83
+    const response = await fetch("http://127.0.0.1:5000/api/getTokenOutput", options);
+    // console.log(response)
+
+    const responseData = await response.json()
+
+    setShowButton(true)
+    setResponse(JSON.stringify(responseData))
+  }
+
+  return (
+    <div className='flex flex-col justify-around h-[50%]'>
+      <div>Token Output</div>
+      <div className="chatbox__chatarea">
+        <textarea className="chatbox__textarea border-2 border-slate-400" onChange={getVal} placeholder="enter text here" rows="3" cols="30"></textarea>
+        {showButton? <><br></br><button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow" onClick={()=>callAPI()}>get response</button></> : <div>Loading...</div>}
+      </div>
+      <div>{response}</div>
+    </div>
+  )
+}
 
 export default ComponentSelector;
