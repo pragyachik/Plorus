81354df451d78840fec7675f9d794896ce719194
Pragya Agrawal <pragyachik@gmail.com>
2023-07-30 19:45:05 +0530

    remove head

--- pages/_app.js
+++ pages/_app.js
@@ -8,29 +8,27 @@ import * as gtag from '../lib/gtag'
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        {/* <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
+      {/* <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
 
-              gtag('config', ${gtag.GA_TRACKING_ID});
-            `,
-          }}
-        /> */}
-          <Script src={"https://www.googletagmanager.com/gtag/js?id="+`${gtag.GA_TRACKING_ID}`} />
-          <Script id="google-analytics">
-            {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-    
-              gtag('config', '${gtag.GA_TRACKING_ID}');
-            `}
-          </Script>
-      </Head>
+            gtag('config', ${gtag.GA_TRACKING_ID});
+          `,
+        }}
+      /> */}
+        <Script src={"https://www.googletagmanager.com/gtag/js?id="+`${gtag.GA_TRACKING_ID}`} />
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+  
+            gtag('config', '${gtag.GA_TRACKING_ID}');
+          `}
+        </Script>
       {/* <Script
         strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
